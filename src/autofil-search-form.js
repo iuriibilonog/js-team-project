79824d3c6384d9635c91f7c3251b7231f0528445
Iurii Bilonog <iuriibilonog@gmail.com>
2021-10-22 12:52:16 +0300

    ch2

--- src/autofil-search-form.js
+++ src/autofil-search-form.js
@@ -8,27 +8,26 @@ const localPosition = localStorage.getItem('localCountryName');
 const inputData = localStorage.getItem('searchData');
 
 (async () => {
-    if (inputData) {
-        searchInput.value = inputData;
-    }
-    if (!localPosition) {
-        autofill().then(data => getDataServer('', data, ''));
-    }
-    else {
-        countryNode.value = localPosition;
-        const localCode = localStorage.getItem('localCountryCode')
-        getDataServer(searchInput.value, localCode, '')
-    }
-})()
+  if (inputData) {
+    searchInput.value = inputData;
+  }
+  if (!localPosition) {
+    autofill().then(data => getDataServer('', data, ''));
+  } else {
+    countryNode.value = localPosition;
+    const localCode = localStorage.getItem('localCountryCode');
+    getDataServer(searchInput.value, localCode, '');
+  }
+})();
 
 async function autofill() {
-    const getCountryCodeByIP = await firstQueryDataServer();
-    const countryCodeValidator = (getCountryCodeByIP) ? getCountryCodeByIP.data.countryCode : 'US';
-    const countryCodeCheck = countries.find(item => item.countryCode === countryCodeValidator);
-    countryNode.value = countryCodeCheck.countryName;
-    if (!localStorage.getItem('localCountryName') && !localStorage.getItem('localCountryCode')) {
-        localStorage.setItem('localCountryName', countryNode.value);
-        localStorage.setItem('localCountryCode', countryCodeValidator);
-    }
-    return countryCodeValidator;
-};
+  const getCountryCodeByIP = await firstQueryDataServer();
+  const countryCodeValidator = getCountryCodeByIP ? getCountryCodeByIP.data.countryCode : 'AU';
+  const countryCodeCheck = countries.find(item => item.countryCode === countryCodeValidator);
+  countryNode.value = countryCodeCheck.countryName;
+  if (!localStorage.getItem('localCountryName') && !localStorage.getItem('localCountryCode')) {
+    localStorage.setItem('localCountryName', countryNode.value);
+    localStorage.setItem('localCountryCode', countryCodeValidator);
+  }
+  return countryCodeValidator;
+}
